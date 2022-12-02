0cfc13cc87af87ee1c2a3b7eaf446d53952ca8da
Katmanjones <88651862+Katmanjones@users.noreply.github.com>
2022-12-02 21:22:17 +0000

    Create ga4_new_users.sql

---
+++ ga4_new_users.sql
@@ -0,0 +1,16 @@
+SELECT
+  #event_date,
+  event_name,
+  #geo.country,
+  #platform,
+  #device.category,
+  #device.mobile_brand_name,
+  #device.operating_system,
+  COUNT(DISTINCT user_pseudo_id)
+
+FROM `measurelab-ga4.analytics_217052535.events_*` 
+
+WHERE _TABLE_SUFFIX BETWEEN '20221101' AND '20221107'
+  AND (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_number') = 1
+
+GROUP BY 1
