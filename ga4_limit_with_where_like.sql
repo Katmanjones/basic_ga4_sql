d6d184b33452824b87b10d8b5daa5e9209e3f6c3
Katmanjones <88651862+Katmanjones@users.noreply.github.com>
2022-12-02 22:44:09 +0000

    Create ga4_limit_with_where_like.sql

---
+++ ga4_limit_with_where_like.sql
@@ -0,0 +1,16 @@
+SELECT
+  #event_date,
+  #event_name,
+  geo.country,
+  #platform,
+  #device.category,
+  #device.mobile_brand_name,
+  #device.operating_system,
+  COUNT(DISTINCT (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id')) AS count_unique_sessions
+
+FROM `measurelab-ga4.analytics_217052535.events_*`
+
+WHERE _TABLE_SUFFIX BETWEEN '20221101' AND '20221107'
+  AND geo.country LIKE 'United%'
+
+GROUP BY 1
