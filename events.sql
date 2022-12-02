be619154cccee709085ff0ae47264b0c839802be
Katmanjones <88651862+Katmanjones@users.noreply.github.com>
2022-12-02 21:30:14 +0000

    Create events.sql

---
+++ events.sql
@@ -0,0 +1,9 @@
+SELECT
+  event_name,
+  COUNT(user_pseudo_id) AS count_of_users
+
+FROM `measurelab-ga4.analytics_217052535.events_*` 
+
+WHERE _TABLE_SUFFIX BETWEEN '20221101' AND '20221107'
+
+GROUP BY 1
