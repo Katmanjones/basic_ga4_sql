53478ba67bdb2f3f593d0a027e426fba1caa50e9
Katmanjones <88651862+Katmanjones@users.noreply.github.com>
2022-12-02 22:47:25 +0000

    Create ga4_limit_with_having_>.sql

---
+++ ga4_limit_with_having_>.sql
@@ -0,0 +1,17 @@
+SELECT
+  #event_date,
+  #event_name,
+  geo.country,
+  #platform,
+  #device.category,
+  #device.mobile_brand_name,
+  #device.operating_system,
+  COUNT(DISTINCT user_pseudo_id) AS count_unique_users
+
+FROM `measurelab-ga4.analytics_217052535.events_*`
+
+WHERE _TABLE_SUFFIX BETWEEN '20221101' AND '20221107'
+
+GROUP BY 1
+
+HAVING COUNT(DISTINCT user_pseudo_id) > 10
